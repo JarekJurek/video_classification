e5b9969dc8b46cc7a67567890c5b568cb4768f6a
Greg Pawlak <grzes.pawlak@gmail.com>
2024-11-27 14:42:35 +0100

    Per frame CNNs update

--- PerFrameCNN.py
+++ PerFrameCNN.py
@@ -1,6 +1,8 @@
 import torch.nn as nn
+from torchvision import models
 
-class PerFrameCNN(nn.Module):
+
+class PerFrameOwn(nn.Module):
     def __init__(self, num_classes=10):
         super(PerFrameCNN, self).__init__()
         self.features = nn.Sequential(
@@ -9,17 +11,27 @@ class PerFrameCNN(nn.Module):
             nn.MaxPool2d(kernel_size=2),
             nn.Conv2d(64, 128, kernel_size=3, padding=1),
             nn.ReLU(),
-            nn.MaxPool2d(kernel_size=2)
+            nn.MaxPool2d(kernel_size=2),
         )
         self.classifier = nn.Sequential(
             nn.Flatten(),
             nn.Linear(128 * 56 * 56, 256),
             nn.ReLU(),
             nn.Dropout(0.5),
-            nn.Linear(256, num_classes)
+            nn.Linear(256, num_classes),
         )
 
     def forward(self, x):
         x = self.features(x)
         x = self.classifier(x)
         return x
+
+
+class PerFrameTrained(nn.Module):
+    def __init__(self, num_classes=10):
+        super(PerFrameTrained, self).__init__()
+        self.backbone = models.efficientnet_v2_s(pretrained=True)
+        self.backbone.fc = nn.Linear(self.backbone.fc.in_features, num_classes)
+
+    def forward(self, x):
+        return self.backbone(x)
