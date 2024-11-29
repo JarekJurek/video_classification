7f42523bdb00d773a03a3e4a93927d20416a81a3
Greg Pawlak <grzes.pawlak@gmail.com>
2024-11-29 19:15:03 +0100

    PerFrameCNN cleanup

--- models/PerFrameCNN.py
+++ models/PerFrameCNN.py
@@ -3,31 +3,6 @@ from torchvision import models
 from torchvision.models import EfficientNet_V2_S_Weights
 
 
-class PerFrameOwn(nn.Module):
-    def __init__(self, num_classes=10):
-        super(PerFrameOwn, self).__init__()
-        self.features = nn.Sequential(
-            nn.Conv2d(3, 64, kernel_size=3, padding=1),
-            nn.ReLU(),
-            nn.MaxPool2d(kernel_size=2),
-            nn.Conv2d(64, 128, kernel_size=3, padding=1),
-            nn.ReLU(),
-            nn.MaxPool2d(kernel_size=2),
-        )
-        self.classifier = nn.Sequential(
-            nn.Flatten(),
-            nn.Linear(128 * 56 * 56, 256),
-            nn.ReLU(),
-            nn.Dropout(0.5),
-            nn.Linear(256, num_classes),
-        )
-
-    def forward(self, x):
-        x = self.features(x)
-        x = self.classifier(x)
-        return x
-
-
 class PerFrameTrained(nn.Module):
     def __init__(self, num_classes=10):
         super(PerFrameTrained, self).__init__()
