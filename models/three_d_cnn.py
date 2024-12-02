f12a18e48a7238ab3307dcb3ccea417d54709bb6
Grzegorz Pawlak <grzes.pawlak@gmail.com>
2024-12-02 18:24:05 +0100

    3D CNN training added

--- models/three_d_cnn.py
+++ models/three_d_cnn.py
@@ -2,9 +2,9 @@ import torch
 import torch.nn as nn
 from torchvision.models.video import r3d_18  # ResNet3D-18
 
-class ResNet3DClassifier(nn.Module):
+class ThreeDCNN(nn.Module):
     def __init__(self, num_classes=10, pretrained=True):
-        super(ResNet3DClassifier, self).__init__()
+        super(ThreeDCNN, self).__init__()
         # Load ResNet3D-18 backbone
         self.backbone = r3d_18(pretrained=pretrained)
         
